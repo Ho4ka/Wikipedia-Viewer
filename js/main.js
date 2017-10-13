f3dc04e7a6f128619b59f3c39c3e6375d794269f
Maksym <m.domaretsky@gmail.com>
2017-10-14 02:28:35 +0300

    js  and style
    add logic and styles

---
+++ js/main.js
@@ -0,0 +1,56 @@
+
+
+document.getElementById('search').addEventListener('click',function(val){
+    let userInput = document.getElementById('userInput');
+    let term = userInput.value;
+    userInput.value = '';
+    let wikiRequest = new XMLHttpRequest();
+    
+    //let url = 'https://en.wikipedia.org/w/api.php?action=query&origin=*&format=json&list=search&prop=extracts&explaintext&exintro&exsentences=1&exlimit=max&srsearch=' + term ;
+    // 'https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&prop=extracts&exlimit=max&explaintext&exintro&exsentences=1&gsrsearch='
+    let url = 'https://en.wikipedia.org/w/api.php?action=opensearch&search='+ term +'&format=json&origin=*';
+    
+    wikiRequest.open('GET', url)
+    wikiRequest.onload = function() {
+        let data = JSON.parse(wikiRequest.responseText);
+     
+        let termArray = data[1];
+        let termArrayText = data[2];
+        let holderForIteration;
+        let output = document.getElementById('output');
+
+        output.innerHTML = '';
+
+        for(let i= 0; i < termArray.length; i++){
+         
+            for (let j = 0; j < termArrayText.length; j++){
+                holderForIteration = termArrayText[i];
+                
+        }
+        output.insertAdjacentHTML('beforeEnd', '<div id="wrapper"><h3 class="title">' +termArray[i]+'</h3>'+'<p>'+holderForIteration+ '</p>'+"</div>");
+    }
+        console.log(data);
+        let switchingToTheLink = document.getElementsByClassName('title');
+        for (let b = 0; b < switchingToTheLink.length; b++) {
+            switchingToTheLink[b].addEventListener('click', function(val){
+                let valueOfTitle = switchingToTheLink[b].innerHTML;
+               
+                window.open("https://en.wikipedia.org/wiki/" + valueOfTitle);
+            })
+        }
+     
+    }
+    wikiRequest.send();
+   
+} )
+
+var hotbod = document.querySelector("body");
+
+function doStuff() {
+    hotbod.className += " animate";
+}
+
+window.onload = function() {
+    doStuff();
+};
+
